9e7b298d024b243fcc835a1f28750a20793321e5
TobiRes <tobias.reski2@gmail.com>
2017-09-20 15:20:49 +0200

    Simple App, shows current location

---
+++ Location/Location.Android/obj/Debug/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,11 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("Location.Droid.MainApplication, Location.Android, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", md52c9a2487c0c12affb46319fd2281c8c4.MainApplication.class, md52c9a2487c0c12affb46319fd2281c8c4.MainApplication.__md_methods);
+		
+	}
+}
---
+++ Location/Location/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Plugin.Geolocator;
+
+namespace Location
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            btnGetLocation.Clicked += BtnGetLocation_Clicked;
+        }
+
+        private async void BtnGetLocation_Clicked(object sender, EventArgs e)
+        {
+            await RetreiveLocation();
+        }
+        private async Task RetreiveLocation()
+        {
+            var locator = CrossGeolocator.Current;
+            locator.DesiredAccuracy = 20;
+
+
+
+            try{
+                var position = await locator.GetPositionAsync(timeoutMilliseconds: 10000);
+
+                txtLat.Text = "Breitengrad: " + position.Latitude.ToString();
+                txtLong.Text = "Längengrad: " + position.Longitude.ToString();
+            }
+          
+            catch (Exception ex)
+            {
+                //will catch any general exception and set message in a string
+                txtLat.Text = ex.Message;
+            }
+
+        }
+    }
+}
